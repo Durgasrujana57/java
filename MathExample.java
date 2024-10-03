b3a0e0902b9a44c7a23a535552494cf694d6bb44
Durgasrujana57 <durgasrujana570@gmail.com>
2024-10-03 16:38:33 +0530

    Add files via upload

---
+++ MathExample.java
@@ -0,0 +1,15 @@
+public class MathExample {
+     public static void main(String[] args) {
+           int x=-10;
+           int y=10;
+           double z=3.14;
+           System.out.println("abs " + Math.abs(x));
+           System.out.println("max " + Math.max(x,y));
+           System.out.println("min " + Math.min(x,y));
+           System.out.println("pow " + Math.pow(2,3));
+           System.out.println("Random " + Math.random());
+           System.out.println("sqrt " + Math.sqrt(y));
+           System.out.println("round " + Math.round(z));
+      }
+   }        
+           
